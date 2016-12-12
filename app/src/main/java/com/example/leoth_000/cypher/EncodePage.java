4a6c7b9f6139d3560bf8d2fe2c5ed9eb589d5564
Matthew Gottschalk <Matthew Gottschalk>
2016-12-12 14:19:06 -0700

    Initial commit

---
+++ app/src/main/java/com/example/leoth_000/cypher/EncodePage.java
@@ -0,0 +1,33 @@
+package com.example.leoth_000.cypher;
+
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EncodePage extends AppCompatActivity {
+    static String TAG = "MainEncode";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encode_page);
+        Log.i(TAG, "Application is running!");
+
+       final EditText editClear = (EditText)(findViewById(R.id.editText_clear));
+       final TextView textCoded = (TextView)(findViewById(R.id.txt_coded));
+        Button btnEncode =(Button)(findViewById(R.id.btnEncode));
+        btnEncode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                String clearText = editClear.getText().toString();
+                textCoded.setText(clearText);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/leoth_000/cypher/Encoder.java
@@ -0,0 +1,8 @@
+package com.example.leoth_000.cypher;
+
+/**
+ * Created by leoth_000 on 12/12/2016.
+ */
+
+public class Encoder {
+}
